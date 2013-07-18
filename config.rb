e30d20c60f174c4cd1e92da7ebd1cbd0c731cd5a
Thomas Reynolds <me@tdreyno.com>
2013-07-18 13:26:31 -0700

    absolute paths on deploy

--- config.rb
+++ config.rb
@@ -9,14 +9,16 @@ activate :directory_indexes
 set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, :with_toc_data => true
 set :markdown_engine, :redcarpet
 
-activate :relative_assets
-
 activate :fjords do |config|
   config.username = Bundler.settings["fjords_username"]
   config.password = Bundler.settings["fjords_password"]
   config.domain = "middlemanapp.com"
   config.gzip_assets = true
   config.cdn = true
+end
+
+configure :development do
+  activate :relative_assets
 end
 
 configure :build do
