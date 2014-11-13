bd980bd4b5ce6138d83f07f7bccdc24bfaabd8ca
Lisa Sy <lisasyis@gmail.com>
2014-11-13 09:52:02 -0800

    Refactor JS syntax and var

--- source/javascripts/docs-nav-animation.js
+++ source/javascripts/docs-nav-animation.js
@@ -1,26 +1,27 @@
 $(function() {
   var $stickyEl = $("[data-waypoint='toc']");
   var $stopEl   = $("[data-waypoint='footer']");
-  var $verticalGutter = $stopEl.outerHeight()
+  var $verticalGutter = $stopEl.outerHeight();
+  var $stickyElHeight = $stickyEl.outerHeight();
 
   $stickyEl.waypoint("sticky", function() {
     $("[data-waypoint='main']").toggleClass("stuck");
   });
 
-  $stopEl.waypoint(function (direction) {
+  $stopEl.waypoint(function(direction) {
     if (direction == "down") {
       var footerOffset = $stopEl.offset();
 
       $stickyEl.css({
         position: "absolute",
-        top: footerOffset.top - $stickyEl.outerHeight() - $verticalGutter
+        top: footerOffset.top - $stickyElHeight - $verticalGutter
       });
     } else if (direction == "up") {
       $stickyEl.attr("style", "");
     }
   }, {
     offset: function () {
-      return $stickyEl.outerHeight();
+      return $stickyElHeight;
     }
   });
 });
