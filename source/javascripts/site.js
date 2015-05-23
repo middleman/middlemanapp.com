a60cf71ce3f9793089d8c41386cb4b3e8197fca5
Max Meyer <dev@fedux.org>
2015-05-23 10:34:55 +0200

    Added anchor.js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,8 +2,10 @@
 //=require "_vendor/highlight.pack"
 //=require "_vendor/waypoints"
 //=require "_vendor/waypoints-sticky"
+//= require anchor-js
 
 //=require "_toc"
 //=require "_docs-nav-animation"
 
 hljs.initHighlightingOnLoad();
+anchors.add();
