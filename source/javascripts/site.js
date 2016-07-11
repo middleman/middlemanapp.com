45e4bc9bebba37c4f36b5cf4839f2f32d40f3ff7
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-07-11 02:47:12 +0200

    Add Algolia's DocSearch (#593)
    * Add Algolia's DocSearch
    
    * remove debug:true from ds.js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,7 @@
 import hljs from 'highlight.js';
 import anchors from 'exports?anchors!anchor-js/anchor';
 import setupDocsNavAnimation from './docs-nav-animation';
+import docsearch from './ds';
 // import setupTOC from './toc';
 
 setupDocsNavAnimation();
@@ -9,3 +10,5 @@ setupDocsNavAnimation();
 hljs.initHighlighting();
 
 anchors.add('.main h2, .main h3, .main h4, .main h5, .main h6');
+
+docsearch();
