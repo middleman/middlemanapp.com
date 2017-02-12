7cf1b8527dc87ada653f6a526650e4fbf5d69e61
Tyson Gach <tyson@tysongach.com>
2017-02-12 15:46:11 -0500

    Use middleman-syntax extension
    - Shows off usage of Middleman extensions
    - The highlighting will happen before compilation, reducing load on the
    front-end in the browser
    
    The Solarized Light theme comes from
    https://gist.github.com/nicolashery/5765395.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,3 @@
-import hljs from 'highlight.js';
 import anchors from 'exports?anchors!anchor-js/anchor';
 import setupDocsNavAnimation from './docs-nav-animation';
 import docsearch from './ds';
@@ -6,8 +5,6 @@ import docsearch from './ds';
 
 setupDocsNavAnimation();
 // setupTOC();
-
-hljs.initHighlighting();
 
 anchors.add('.main h2, .main h3, .main h4, .main h5, .main h6');
 
