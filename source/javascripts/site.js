03c2eb416d3c3c056d2305a836f04242566086ea
Tyson Gach <tyson@tysongach.com>
2017-02-14 12:29:54 -0800

    Remove unused JavaScript (#651)
    This JavaScript was commented out in
    https://github.com/middleman/middleman-guides/commit/7a03bc267c7fd2ca3c7b0e38f8710071e1ef6b7b.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,8 @@
 import anchors from 'exports?anchors!anchor-js/anchor';
 import setupDocsNavAnimation from './docs-nav-animation';
 import docsearch from './ds';
-// import setupTOC from './toc';
 
 setupDocsNavAnimation();
-// setupTOC();
 
 anchors.add('.main h2, .main h3, .main h4, .main h5, .main h6');
 
