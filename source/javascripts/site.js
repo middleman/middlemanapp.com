40d7656b0f7b8e4102f3a9adb58147009caf6eb8
Tyson Gach <tyson@tysongach.com>
2017-02-27 09:37:18 -0500

    Rebuild layouts

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,6 @@
 import anchors from 'exports?anchors!anchor-js/anchor';
-import setupDocsNavAnimation from './docs-nav-animation';
 import docsearch from './ds';
 
-setupDocsNavAnimation();
-
-anchors.add('.main h2, .main h3, .main h4, .main h5, .main h6');
+anchors.add('.js-anchor-links h2, .js-anchor-links h3, .js-anchor-links h4, .js-anchor-links h5, .js-anchor-links h6');
 
 docsearch();
